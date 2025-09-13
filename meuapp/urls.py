1f780e137cfa5fdb83db31ea49f2b291f308d3c8
Gabriel Furtado Ramalho <emailsopratwitch@gmail.com>
2025-09-13 20:37:32 -0300

    ✨ feat: Funções CRUD completas
    Funções CRUD completas

--- meuapp/urls.py
+++ meuapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('listar/', views.listar_livros, name='listar'),
+    path('create/', views.criar_livro, name='create'),
+    path('delete/<int:id>/', views.deletar_livro, name='deletar_livro'),
+    path('update/<int:pk>/', views.atualizar_livro, name='atualizar_livro'),
 ]
--- meuapp/views.py
+++ meuapp/views.py
@@ -6,5 +6,40 @@ import requests
 def home(request):
     return render(request, 'home.html')
 
+def listar_livros(request):
+    livros = Livros.objects.all()
+    return render(request, 'list.html', {'livros': livros})
+
+def criar_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
+    else:
+        form = LivroForm()
+    return render(request, 'criar_livro.html', {'livros': form})
+
+
+def deletar_livro(request, id):
+    livro = Livros.objects.get(id=id)
+    if request.method == 'POST':
+        livro.delete()
+        return redirect('listar')
+    return render(request, 'delete.html', {'livro': livro})
+
+def atualizar_livro(request, pk):
+    livro = Livros.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
+    else:
+        form = LivroForm(instance=livro)
+    return render(request, 'criar_livro.html', {'livros': form})
+
+
+
 
 
