ff5315129ec1d4822573aeb9b190a1a5a4c712ae
Gabriel Furtado Ramalho <emailsopratwitch@gmail.com>
2025-09-14 01:41:35 -0300

    ✨ feat: Adicionei Api
    Adicionei API e corrigi alguns erros

--- meuapp/models.py
+++ meuapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Livros(models.Model):
-        titulo = models.CharField(max_length=100)
-        autor = models.CharField(max_length=100)
-        data_publicacao = models.DateField()
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    data_publicacao = models.DateField(null=True, blank=True)
+    capa_url = models.URLField(max_length=200, null=True, blank=True)
 
-        def __str__(self):
-         return self.titulo
+    def __str__(self):
+        return self.titulo
--- meuapp/views.py
+++ meuapp/views.py
@@ -2,11 +2,36 @@ from django.shortcuts import render, redirect
 from .models import Livros
 from .forms import LivroForm
 import requests
+from datetime import datetime
 
 def home(request):
     return render(request, 'home.html')
 
 def listar_livros(request):
+    livros = Livros.objects.all()
+    if "q" in request.GET:
+        termo = request.GET["q"]
+        url = f"https://openlibrary.org/search.json?title={termo}"
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            dados = resposta.json()
+            for item in dados["docs"][:5]:
+                titulo = item.get("title")
+                autor = ", ".join(item.get("author_name", [])) if item.get("author_name") else "Desconhecido"
+                ano = item.get("first_publish_year")
+                data_publicacao = None
+                if ano:
+                    data_publicacao = datetime(ano, 1, 1).date()
+                livro_obj, created = Livros.objects.get_or_create(
+                    titulo=titulo,
+                    defaults={'autor': autor, 'data_publicacao': data_publicacao}
+                )
+                isbn_list = item.get("isbn", [])
+                if isbn_list:
+                    livro_obj.capa_url = f"https://covers.openlibrary.org/b/isbn/{isbn_list[0]}-M.jpg"
+                else:
+                    livro_obj.capa_url = None
+                livro_obj.save()
     livros = Livros.objects.all()
     return render(request, 'list.html', {'livros': livros})
 
@@ -18,8 +43,7 @@ def criar_livro(request):
             return redirect('listar')
     else:
         form = LivroForm()
-    return render(request, 'criar_livro.html', {'livros': form})
-
+    return render(request, 'criar_livro.html', {'form': form})
 
 def deletar_livro(request, id):
     livro = Livros.objects.get(id=id)
@@ -37,9 +61,4 @@ def atualizar_livro(request, pk):
             return redirect('listar')
     else:
         form = LivroForm(instance=livro)
-    return render(request, 'criar_livro.html', {'livros': form})
-
-
-
-
-
+    return render(request, 'criar_livro.html', {'form': form})
